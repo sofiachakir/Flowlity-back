f7351ab3c7dcad583913206f91e885adcb477292
sofiachakir <sofiachakir@gmail.com>
2020-08-05 18:19:12 +0200

    add products controllers, namespace api/v1 and render json in controller methods

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,13 @@
+module Api::V1
+	class ProductsController < ApplicationController
+	  def index
+	  	@products = Product.all
+	  	render json: @products
+	  end
+
+	  def show
+	  	@product = Product.find(params[:id])
+	  	render json: @product
+	  end
+	end
+end
