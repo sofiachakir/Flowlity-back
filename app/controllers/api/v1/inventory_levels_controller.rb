b075e0d45577d0999c70dae47c701646e1ce12e9
sofiachakir <sofiachakir@gmail.com>
2020-08-06 14:08:36 +0200

    add order to inventory levels

--- app/controllers/api/v1/inventory_levels_controller.rb
+++ app/controllers/api/v1/inventory_levels_controller.rb
@@ -3,7 +3,7 @@ module Api::V1
 		# Get /api/v1/products/[:id]/inventory_levels
 	  def index
 	  	@product = Product.find(params[:product_id])
-	  	@inventory_levels = @product.inventory_levels
+	  	@inventory_levels = @product.inventory_levels.order(:date)
 	  	render json: @inventory_levels
 	  end
 	end
